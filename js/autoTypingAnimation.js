c101e16338bc1a6267da916a48992e4bd41275dc
CandraAllan <115175643+CandraAllan@users.noreply.github.com>
2022-10-10 01:14:09 +0700

    Add files via upload

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Halo, Saya Candra Allan Novanto</span> Lulusan dari Universitas Semarang dengan program studi S-1 Teknik Informatika dengan IPK 3.72. Memiliki pengalaman kerja magang pada perusahaan CV. Aflakh Jaya yang bergerak pada bidang konstruksi dan furniture. Saya juga memiliki pengalaman magang pada perusahaan PT. DES Teknologi Informasi yang bergerak pada bidang Internet Service Provider <br><br> Saya memiliki minat yang sangat besar pada bidang IT dengan memiliki rasa tanggung jawab yang tinggi serta disiplin dalam bekerja dan mampu memanajemen pekerjaan dengan baik. Saya memiliki kemampuan komunikasi yang baik, mampu belajar sesuatu yang baru dan cepat dalam menanggapi arahan"
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
